5456896b081b3fbe05eb4f09e97e293b98674313
Phillip Malboeuf <phil@phils.computer>
2020-03-20 10:42:29 -0400

    map page

--- scripts/things.js
+++ scripts/things.js
@@ -1,3 +1,6 @@
+const last = '#AB45FF'
+const colors = ['#FF5757', '#FE9900', '#FEED19', '#7AFF58', '#00F5E0', '#0094FE', last]
+
 console.log('Things')
 
 document.querySelectorAll('.sqs-block-button-element--large').forEach(element => {
@@ -7,3 +10,60 @@ document.querySelectorAll('.sqs-block-button-element--large').forEach(element =>
     element.innerHTML += '<span class="script">' + texts[1] + '</span>'
   }
 })
+
+if (document.body.id === 'collection-5e63dd6d4f3cd622ab519e5e') {
+  document.querySelectorAll('.sqs-block-html h1').forEach((element, index) => {
+    element.style.color = colors[index]
+  })
+
+  document.querySelectorAll('.sqs-block-summary-v2').forEach((element, index) => {
+    element.querySelectorAll('.summary-item').forEach((item) => {
+      item.style.borderColor = colors[index]
+    })
+  })
+}
+
+
+function things() {
+
+  document.querySelectorAll('[data-map]').forEach((element, index) => {
+    const collection = element.getAttribute('data-map')
+    fetch(`/${collection}?format=json`)
+      .then(response => response.json())
+      .then(data => {
+        const map = new google.maps.Map(element, {
+          styles: [ { "elementType": "geometry", "stylers": [ { "color": "#f5f5f5" } ] }, { "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#616161" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#f5f5f5" } ] }, { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [ { "color": "#bdbdbd" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#eeeeee" } ] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [ { "color": "#757575" } ] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [ { "color": "#e5e5e5" } ] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [ { "color": "#9e9e9e" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "color": "#ffffff" } ] }, { "featureType": "road.arterial", "elementType": "labels.text.fill", "stylers": [ { "color": "#757575" } ] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "color": "#dadada" } ] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [ { "color": "#616161" } ] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "color": "#9e9e9e" } ] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [ { "color": "#e5e5e5" } ] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [ { "color": "#eeeeee" } ] }, { "featureType": "water", "elementType": "geometry", "stylers": [ { "color": "#c9c9c9" } ] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#9e9e9e" } ] } ]
+        })
+        let bounds = new google.maps.LatLngBounds()
+
+        data.items.forEach(item => {
+          const marker = new google.maps.Marker({
+            map,
+            animation: google.maps.Animation.DROP,
+            title: item.location.addressTitle,
+            position: {
+              lat: item.location.mapLat,
+              lng: item.location.mapLng,
+            }
+          })
+          const infowindow = new google.maps.InfoWindow({
+            content: item.excerpt
+          })
+          const box = document.querySelector(`a[href~="/${collection}/${item.urlId}"]`).parentElement.parentElement
+          marker.addListener('click', () => {
+            infowindow.open(map, marker)
+            
+            box.parentElement.parentElement.parentElement.scrollTo({ top: box.offsetTop - 33 })
+          })
+
+          bounds.extend(marker.getPosition())
+        })
+
+        map.fitBounds(bounds)
+      })
+    
+  })
+}
+
+window.addEventListener('load', things)
+
